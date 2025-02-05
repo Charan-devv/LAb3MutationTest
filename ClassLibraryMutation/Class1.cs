6ac50e6df37e160bda73779e01e7f6256da63fe1
root <root@Luffy.>
2025-02-05 16:39:30 -0500

    Add Class library which consist the test case

---
+++ ClassLibraryMutation/Class1.cs
@@ -0,0 +1,31 @@
+﻿//Lab3: Mutation Test
+//Name: Charankanth Reddy Chinthala
+
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Lab3Mutation;
+
+namespace TriangleTest
+{
+    [TestFixture]
+    public class Trianglestest
+    {
+        [Test]
+        public void TestValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstSide = 70;
+            int secondSide = 60;
+            int thirdSide = 40;
+
+            string expected = "The triangle is an isosceles triangle.";
+
+            // Act
+            string actual = Triangles.TriangleIsosceles(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
